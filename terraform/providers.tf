90c21f5536e0b5c544a8cb64750927077ef15998
adusei2023 <samuel.boateng01@yahoo.com>
2025-05-30 12:57:35 +0000

    Add aws_region variable declaration to fix providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,3 +11,9 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
